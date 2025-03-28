2f920c9819305921f45723bb69f1206c962f3267
unknown <farismmk@gmail.com>
2025-03-28 03:13:43 -0700

    use xbuild

--- build.sh
+++
@@ -1,5 +0,0 @@
-cmake -S . -B cJSON/build -DCMAKE_BUILD_TYPE=Debug
-cmake --build cJSON/build --config Debug
-
-cmake -S . -B cJSON/build -DCMAKE_BUILD_TYPE=Release
-cmake --build cJSON/build --config Release
